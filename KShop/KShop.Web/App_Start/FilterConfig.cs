66e39ff6fe061d33bda94c8e5d1668d08366565a
VP\khangpv <khangpv@zopost.vn>
2018-03-01 09:30:23 +0700

    init

---
+++ KShop/KShop.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KShop.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
